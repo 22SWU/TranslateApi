1bb4ead2859c3a8b3718c2b0be804667aa7dc593
Minyoung-Lee <leemy08@naver.com>
2022-02-07 23:18:26 +0900

    [ADD] Spinner 에서 언어 선택

--- app/src/main/java/com/example/howltranslator/MainActivity.kt
+++ app/src/main/java/com/example/howltranslator/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import com.example.howltranslator.databinding.ActivityMainBinding
@@ -21,10 +23,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         option = FirebaseTranslatorOptions.Builder()   // 번역할 언어 설정
-            .setSourceLanguage(FirebaseTranslateLanguage.EN)
-            .setTargetLanguage(FirebaseTranslateLanguage.KO)
-        translater = FirebaseNaturalLanguage.getInstance().getTranslator(option!!.build())    // 번역 해 주는 코드
-        translater?.downloadModelIfNeeded()     // option 에 단어를 넣어 주고 번역 할 언어를 다운받아야 함
+            //.setSourceLanguage(FirebaseTranslateLanguage.EN)
+            //.setTargetLanguage(FirebaseTranslateLanguage.KO)
+//        translater = FirebaseNaturalLanguage.getInstance().getTranslator(option!!.build())    // 번역 해 주는 코드
+//        translater?.downloadModelIfNeeded()     // option 에 단어를 넣어 주고 번역 할 언어를 다운받아야 함
 
         binding.inputText.addTextChangedListener(object : TextWatcher{
             override fun afterTextChanged(p0: Editable?) {
@@ -43,6 +45,43 @@ class MainActivity : AppCompatActivity() {
 
 
         })
+        // 왼쪽 스피너에 이벤트
+        binding.fromSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (p2 == 0){
+                    // 영어
+                    option?.setSourceLanguage(FirebaseTranslateLanguage.EN)
 
+                } else if (p2 == 1){
+                    // 한국어
+                    option?.setSourceLanguage(FirebaseTranslateLanguage.KO)
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        binding.toSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                if (p2 == 0){
+                    // 영어
+                    option?.setTargetLanguage(FirebaseTranslateLanguage.EN)
+
+                } else if (p2 == 1){
+                    // 한국어
+                    option?.setTargetLanguage(FirebaseTranslateLanguage.KO)
+                }
+                translater = FirebaseNaturalLanguage.getInstance().getTranslator(option!!.build())    // 번역 해 주는 코드
+                translater?.downloadModelIfNeeded()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+
+        }
     }
 }
